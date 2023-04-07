9683a38d92461f96b7f9ed1baad4ecb36ec1d615
sihyeokpark <exon.indiv@gmail.com>
2023-04-07 22:08:50 +0900

    Update:

--- app/src/pages/components/Navigator.tsx
+++ app/src/pages/components/Navigator.tsx
@@ -1,16 +1,17 @@
 import styles from '@/styles/Navigator.module.css'
+import Link from 'next/link'
 
 export default function Navigator() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.left}>
-        <img src='./logo.png'></img>
+        <Link href="/"><img src='./logo.png'></img></Link>
         {/* <a className={styles.logo}>Dimi6</a> */}
       </div>
       <div className={styles.right}>
-        <a>Home</a>
-        <a>Shop</a>
-        <img src="./exon.png"></img>
+        <Link href="/shop">Shop</Link>
+        <Link href="/login">Login</Link>
+        {/* <img src="./exon.png"></img> */}
       </div>
     </nav>
   )
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useRouter } from 'next/router'
+
 import styles from '@/styles/Home.module.css'
 
 import Navigator from './components/Navigator'
