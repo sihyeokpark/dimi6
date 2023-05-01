98b3f52f91f968db1089d52e223bc464b28ee87e
sihyeokpark <exon.indiv@gmail.com>
2023-05-01 17:54:19 +0900

    Update: SQLite to MySQL

---
+++ app/prisma/migrations/20230501083931_create_categories/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `point` INTEGER NOT NULL DEFAULT 0,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
