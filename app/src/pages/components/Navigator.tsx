896b99a389756e93f0e2e236210a6477593aa220
sihyeokpark <exon.indiv@gmail.com>
2023-05-05 20:47:58 +0900

    Update: Login and Logout

--- app/src/pages/components/Navigator.tsx
+++ app/src/pages/components/Navigator.tsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
+
 import styles from '@/styles/Navigator.module.css'
 import Link from 'next/link'
 
 export default function Navigator() {
+  const [isLogin, setIsLogin] = useState(false)
+
+  useEffect(() => {
+    verify()
+  }, [])
+
+  async function verify() {
+    const token = localStorage.getItem('token')
+    if (token === '') return false
+    const data = await (await fetch(`/api/verify?token=${token}`)).json()
+    if (data.StatusCode == 200) setIsLogin(true)
+    else setIsLogin(false)
+  }
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.left}>
@@ -10,7 +26,8 @@ export default function Navigator() {
       </div>
       <div className={styles.right}>
         <Link href="/shop">Shop</Link>
-        <Link href="/login">Login</Link>
+        { isLogin && <a href="/" onClick={() => localStorage.removeItem('token')}>Logout</a> }
+        { !isLogin && <Link href="/login">Login</Link> }
         {/* <img src="./exon.png"></img> */}
       </div>
     </nav>
