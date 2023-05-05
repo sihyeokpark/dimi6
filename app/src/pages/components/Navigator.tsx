6458409f0b47e3cbad5ec82ad37cc49d3ee9f495
sihyeokpark <exon.indiv@gmail.com>
2023-05-05 22:23:45 +0900

    Update: Navigator admin page

--- app/src/pages/components/Navigator.tsx
+++ app/src/pages/components/Navigator.tsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 
 import styles from '@/styles/Navigator.module.css'
+import adminMembers from '../../data/admin.json'
 import Link from 'next/link'
 
 export default function Navigator() {
   const [isLogin, setIsLogin] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false)
 
   useEffect(() => {
     verify()
@@ -14,7 +16,11 @@ export default function Navigator() {
     const token = localStorage.getItem('token')
     if (token === '') return false
     const data = await (await fetch(`/api/verify?token=${token}`)).json()
-    if (data.StatusCode == 200) setIsLogin(true)
+    if (data.StatusCode == 200) {
+      setIsLogin(true)
+      if (adminMembers.indexOf(data.name) != -1) setIsAdmin(true)
+      else setIsAdmin(false)
+    }
     else setIsLogin(false)
   }
 
@@ -25,6 +31,7 @@ export default function Navigator() {
         {/* <a className={styles.logo}>Dimi6</a> */}
       </div>
       <div className={styles.right}>
+        { isAdmin && <Link href='/admin'>Admin</Link> }
         <Link href="/shop">Shop</Link>
         { isLogin && <a href="/" onClick={() => localStorage.removeItem('token')}>Logout</a> }
         { !isLogin && <Link href="/login">Login</Link> }
