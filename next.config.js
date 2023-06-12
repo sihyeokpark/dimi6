4e201b36b2a8bb92628cdba9f2be8f2ce2f618fd
sihyeokpark <exon.indiv@gmail.com>
2023-06-12 18:10:34 +0900

    Update: csp

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+  frame-src giscus.app
+`
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+]
+
+async function headers() {
+  return [
+    {
+      source: '/(.*)',
+      headers: securityHeaders,
+    },
+  ]
+}
+
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
@@ -7,3 +35,4 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = headers
