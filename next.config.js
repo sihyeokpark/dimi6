a55eba372710e94b4a26a15ed28489017204dc4c
sihyeokpark <exon.indiv@gmail.com>
2023-06-22 10:23:10 +0900

    🐛 : CSP bug..

--- next.config.js
+++ next.config.js
@@ -1,38 +1,38 @@
-/** @type {import('next').NextConfig} */
-const ContentSecurityPolicy = `
-default-src 'self';
-script-src 'self' ;
-style-src 'self';
-img-src * blob: data:;
-media-src 'none';
-connect-src *;
-font-src 'self';
-object-src 'none';
-`
+// /** @type {import('next').NextConfig} */
+// const ContentSecurityPolicy = `
+// default-src 'self';
+// script-src 'self' ;
+// style-src 'self';
+// img-src * blob: data:;
+// media-src 'none';
+// connect-src *;
+// font-src 'self';
+// object-src 'none';
+// `
 
-const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-]
+// const securityHeaders = [
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+//   {
+//     key: 'Content-Security-Policy',
+//     value: ContentSecurityPolicy.replace(/\n/g, ''),
+//   },
+// ]
 
-async function headers() {
-  return [
-    {
-      source: '/(.*)',
-      headers: securityHeaders,
-    },
-  ]
-}
+// async function headers() {
+//   return [
+//     {
+//       source: '/(.*)',
+//       headers: securityHeaders,
+//     },
+//   ]
+// }
 
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
     appDir: true,
   },
-  headers: headers
+  // headers: headers
 }
 
 module.exports = nextConfig
