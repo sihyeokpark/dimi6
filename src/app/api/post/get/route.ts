984e1d77a804fa68023a223653862ca45eef72bd
sihyeokpark <exon.indiv@gmail.com>
2023-06-28 18:01:19 +0900

    🐛 REfix: /api/post/get

--- src/app/api/post/get/route.ts
+++ src/app/api/post/get/route.ts
@@ -5,6 +5,6 @@ import client from '@/lib/client'
 export async function GET(req: Request) {
   const posts = await client.post.findMany()
   return NextResponse.json(posts, { status: 200, headers: {
-    'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
+    'cache-control': 'no-store',
   }, })
 }
