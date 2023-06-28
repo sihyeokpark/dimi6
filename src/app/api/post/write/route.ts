ece9f517336d1246cd9771fe6ae655adea52a858
sihyeokpark <exon.indiv@gmail.com>
2023-06-28 18:25:49 +0900

    Fix: post id

--- src/app/api/post/write/route.ts
+++ src/app/api/post/write/route.ts
@@ -6,10 +6,13 @@ import client from '@/lib/client'
 export async function POST(req: Request, res: Response) {
   const body = await req.json()
 
+  const lastId = (await client.post.findFirst())?.id || 0
+
   const name = JSON.parse(base64url.decode(body.token.split('.')[1])).name
 
   await client.post.create({
     data: {
+      id: lastId+1,
       title: body.title,
       content: body.content,
       date: new Date(),
