df8cbcd752cc53d1ffad6ff3a35c1203fdd36075
sihyeokpark <exon.indiv@gmail.com>
2023-06-28 17:57:10 +0900

    🐛 Fix: /api/post/get

--- src/app/api/post/get/route.ts
+++ src/app/api/post/get/route.ts
@@ -2,7 +2,9 @@ import { NextResponse } from 'next/server'
 
 import client from '@/lib/client'
 
-export async function GET() {
+export async function GET(req: Request) {
   const posts = await client.post.findMany()
-  return NextResponse.json(posts, { status: 200 })
+  return NextResponse.json(posts, { status: 200, headers: {
+    'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
+  }, })
 }
